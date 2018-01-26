a46d10703d83a98275bd34931a9d0e2217d81ec2
Nilton Teixeira <niltonfrederico@protonmail.com>
2018-01-26 01:12:49 -0200

    Menu and Header component elaborated

---
+++ src/components/Template/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- Header ================================================== -->
+  <header id="home">
+
+    <top-menu></top-menu>
+
+    <div class="row banner">
+      <div class="banner-text">
+        <h1 class="responsive-headline">Olá! Eu sou Nilton Teixeira.</h1>
+        <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span>
+          creating awesome and
+          effective visual identities for companies of all sizes around the globe. Let's <a class="smoothscroll"
+                                                                                            href="#about">start
+            scrolling</a>
+          and learn more <a class="smoothscroll" href="#about">about me</a>.</h3>
+        <hr/>
+        <ul class="social">
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+          <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+          <li><a href="#"><i class="fa fa-skype"></i></a></li>
+        </ul>
+      </div>
+    </div>
+
+    <p class="scrolldown">
+      <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+    </p>
+
+  </header>
+</template>
+
+<script>
+
+import Menu from './Menu'
+
+export default {
+  name: 'hero',
+  components: {
+    'top-menu': Menu
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
