b8da52c09f11634d30eb5a38cf07b47d68cc2ef9
Nilton Teixeira <niltonfrederico@protonmail.com>
2018-01-26 01:49:06 -0200

    Texto inicial

--- src/components/Template/Header.vue
+++ src/components/Template/Header.vue
@@ -10,21 +10,18 @@
           <span>Olá! Eu sou</span> <br>
           Nilton Teixeira.
         </h1>
-        <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span>
-          creating awesome and
-          effective visual identities for companies of all sizes around the globe. Let's <a class="smoothscroll"
-                                                                                            href="#about">start
-            scrolling</a>
-          and learn more <a class="smoothscroll" href="#about">about me</a>.</h3>
+        <h3>Eu sou um <span>desenvolvedor full-stack</span> estacionado em Ribeirão Preto/SP.
+          Acredito em <span>desenvolver para humanos</span>, deixando de lado crenças engessadas e colocando
+          <span>conceito sobre ferramenta</span>. <br>
+          Ficou curioso? <a class="smoothscroll" href="#about">Me conheça melhor logo abaixo!</a>
+        </h3>
         <hr/>
         <ul class="social">
-          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+          <li><a href="#"><i class="fa fa-github"></i></a></li>
           <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-          <li><a href="#"><i class="fa fa-skype"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-envelope"></i></a></li>
         </ul>
       </div>
     </div>
@@ -37,7 +34,6 @@
 </template>
 
 <script>
-
 import Menu from './Menu'
 
 export default {
