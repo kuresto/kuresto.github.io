1dab645e56977845ba9daaf3868094e065ca8e4c
Nilton Teixeira <niltonfrederico@protonmail.com>
2018-01-28 21:35:39 -0200

    Added seeder for "filosofia"

--- src/components/Template/Projects.vue
+++ src/components/Template/Projects.vue
@@ -6,7 +6,7 @@
 
     <!-- Education
     ----------------------------------------------- -->
-    <div class="row education">
+    <div class="row education" v-if="filosofia.conteudo.length !== undefined">
 
       <div class="three columns header-col">
         <h1><span>Filosofia</span></h1>
@@ -18,34 +18,11 @@
 
           <div class="twelve columns">
 
-            <h3>University of Life</h3>
-            <p class="info">Master in Graphic Design <span>&bull;</span> <em class="date">April 2007</em></p>
+            <h3>{{ filosofia.quote }}</h3>
+            <p class="info">{{ filosofia.origin }}</p>
 
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-              massa.
-              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-              Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis
-              pretium.
-            </p>
-
-          </div>
-
-        </div> <!-- item end -->
-
-        <div class="row item">
-
-          <div class="twelve columns">
-
-            <h3>School of Cool Designers</h3>
-            <p class="info">B.A. Degree in Graphic Design <span>&bull;</span> <em class="date">March 2003</em></p>
-
-            <p>
-              This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-              Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-              nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
-              ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat
+              {{ filosofia.conteudo }}
             </p>
 
           </div>
@@ -149,6 +126,9 @@ export default {
   computed: {
     projects () {
       return YAML.load('/static/seed/historico.yml')
+    },
+    filosofia () {
+      return YAML.load('/static/seed/filosofia.yml')
     }
   }
 
