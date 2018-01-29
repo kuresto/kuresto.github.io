112c65f01d744a22fe8a45a78848bc42c73192de
Nilton Teixeira <niltonfrederico@protonmail.com>
2018-01-28 22:59:02 -0200

    Added Footer to the components

--- src/components/Template/Skills.vue
+++ src/components/Template/Skills.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'TheFooter'
+  name: 'skills'
 }
 </script>
 
--- src/components/Template/TheFooter.vue
+++ src/components/Template/TheFooter.vue
@@ -1,13 +1,49 @@
 <template>
-  <div></div>
+  <!-- footer ================================================= -->
+  <footer>
+
+    <div class="row">
+
+      <div class="twelve columns">
+
+        <ul class="social-links">
+          <li><a href="https://github.com/kuresto" title="Meu GitHub" target="_blank"><i class="fa fa-github"></i></a>
+          </li>
+          <li><a href="https://www.linkedin.com/in/nilton-teixeira-22069a106" target="_blank" title="Meu LinkedIn">
+            <i class="fa fa-linkedin"></i>
+          </a></li>
+          <li><a href="https://twitter.com/whoiskuresto" target="_blank" title="Meu Twitter"><i
+            class="fa fa-twitter"></i></a></li>
+          <li><a href="https://www.facebook.com/kuresto"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="mailto:niltonfrederico@gmail.com" title="Me envie um e-mail!" target="_blank"><i
+            class="fa fa-envelope"></i></a></li>
+        </ul>
+
+        <ul class="copyright">
+          <li>2018 Developed <span class="grey">with</span>out coffee</li>
+          <li>Based on <a href="https://www.styleshout.com/free-templates/ceevee/" target="_blank"
+                          title="CeeVee Template">CeeVee</a>
+            by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+        </ul>
+
+      </div>
+
+      <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i
+        class="fa fa-fw fa-chevron-up"></i></a></div>
+
+    </div>
+
+  </footer> <!-- Footer End-->
 </template>
 
 <script>
 export default {
-  name: 'testimonials'
+  name: 'TheFooter'
 }
 </script>
 
 <style scoped>
-
+  .grey {
+    color: #525252
+  }
 </style>
