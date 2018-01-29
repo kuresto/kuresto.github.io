2fe198fbc57d3e2839ad325792541704ac9fb8ee
Nilton Teixeira <niltonfrederico@protonmail.com>
2018-01-28 23:03:56 -0200

    Added skills template

--- src/components/Template/Skills.vue
+++ src/components/Template/Skills.vue
@@ -1,5 +1,41 @@
 <template>
-  <div></div>
+  <!-- Portfolio Section
+    ================================================== -->
+  <section id="portfolio">
+
+    <div class="row">
+
+      <div class="twelve columns collapsed">
+
+        <h1>Check Out Some of My Works.</h1>
+
+        <!-- portfolio-wrapper -->
+        <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-thirds cf">
+
+          <div class="columns portfolio-item">
+            <div class="item-wrap">
+
+              <a href="#modal-01" title="">
+                <img alt="" src="images/portfolio/coffee.jpg">
+                <div class="overlay">
+                  <div class="portfolio-item-meta">
+                    <h5>Coffee</h5>
+                    <p>Illustrration</p>
+                  </div>
+                </div>
+                <div class="link-icon"><i class="icon-plus"></i></div>
+              </a>
+
+            </div>
+          </div> <!-- item end -->
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
 </template>
 
 <script>
