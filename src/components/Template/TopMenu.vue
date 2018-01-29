50a9ea81e800209aeaad3c2068d61305ed70f539
Nilton Teixeira <niltonfrederico@protonmail.com>
2018-01-29 18:26:27 -0200

    Menu fades

--- src/components/Template/TopMenu.vue
+++ src/components/Template/TopMenu.vue
@@ -36,20 +36,33 @@ export default {
         }, false)
       }
     }
-    /* $('.smoothscroll').on('click', function (e) {
-      e.preventDefault()
 
-      var target = this.hash,
-        $target = $(target)
-
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-        window.location.hash = target
-      })
-    }) */
     // fade menu
+    window.addEventListener('scroll', function () {
+      let h, y, nav
 
+      h = document.getElementsByTagName('header')[0].clientHeight
+      y = document.documentElement.scrollTop
+      nav = document.getElementById('nav-wrap')
+
+      if ((y > h * 0.90) && (y < h) && (window.outerWidth > 768)) {
+        nav.className += ' fadeOut animated'
+        nav.classList.remove('fadeIn')
+        nav.classList.remove('animated')
+      } else {
+        if (y < h * 0.90) {
+          nav.classList.remove('opaque')
+          nav.classList.remove('fadeOut')
+          nav.classList.remove('animated')
+          nav.className += ' fadeIn animated'
+        } else {
+          nav.className += ' opaque'
+          nav.className += ' fadeIn animated'
+          nav.classList.remove('fadeOut')
+          nav.classList.remove('animated')
+        }
+      }
+    }, false)
     // menu highlight
   }
 }
